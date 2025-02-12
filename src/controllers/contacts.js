3424512b0a8467afa80b6034c7561bdf1f0d4c0d
Anastasiia Bulba <fedinanastya@gmail.com>
2025-02-12 08:48:23 +0200

    fix cloudinary true

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -59,7 +59,7 @@ export const createContactController = async (req, res, next) => {
 
     let photoUrl;
     if (photo) {
-      if (getEnvVar('ENABLE_CLOUDINARY') === true) {
+      if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
         photoUrl = await saveFileToCloudinary(photo);
       } else {
         photoUrl = await saveFileToUploadDir(photo);
@@ -91,7 +91,7 @@ export const patchContactController = async (req, res, next) => {
   let photoUrl;
 
   if (photo) {
-    if (getEnvVar('ENABLE_CLOUDINARY') === true) {
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
       photoUrl = await saveFileToCloudinary(photo);
     } else {
       photoUrl = await saveFileToUploadDir(photo);
