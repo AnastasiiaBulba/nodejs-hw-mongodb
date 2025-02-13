cb3d6b5d88432cd7bee1010c8e5b108a51217650
Anastasiia Bulba <fedinanastya@gmail.com>
2025-02-13 20:19:38 +0200

    swagger

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 // далі встановлюю порт
 const PORT = process.env.PORT || 3000;
@@ -34,6 +35,9 @@ export function setupServer() {
 
   // app.use(contactsRouter);
   app.use(router);
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
